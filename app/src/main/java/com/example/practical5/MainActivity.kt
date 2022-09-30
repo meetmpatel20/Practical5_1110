16eaf3488c56c1ec4d6903959ed58da6e1863368
NirmalPatel17 <nirmaldpatel2003@gmail.com>
2022-09-30 22:13:12 +0530

    Edit two files

--- app/src/main/java/com/example/practical5/MainActivity.kt
+++ app/src/main/java/com/example/practical5/MainActivity.kt
@@ -5,14 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.SeekBar
-import android.widget.TextView
-import android.widget.Toast
+
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var play_btn : ImageButton
     lateinit var stop_btn : ImageButton
     lateinit var ref_seekbar : SeekBar
+    lateinit var forwardbutton : ImageButton
+    lateinit var backwardbutton : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,21 +21,45 @@ class MainActivity : AppCompatActivity() {
 
         play_btn = findViewById(R.id.btn_play)
         stop_btn = findViewById(R.id.btn_stop)
-        ref_seekbar = findViewById(R.id.seekbar)
+        forwardbutton = findViewById(R.id.btn_skip2)
+        backwardbutton = findViewById(R.id.btn_skip1)
 
+        var count = 0
         play_btn.setOnClickListener {
-            intent = Intent(this,MyService::class.java)
-            intent.putExtra("Service1","play")
-            play_btn.setImageResource(R.drawable.ic_baseline_pause_24)
-            startService(intent)
+            count++
+            Intent(applicationContext, MyService::class.java).putExtra(
+                MyService.Service1,
+                MyService.PlayPause
+            ).apply {
+                startService(this)
+            }
+            if (count % 2 != 0) {
+                play_btn.setImageResource(R.drawable.ic_baseline_pause_24)
+            } else {
+                play_btn.setImageResource(R.drawable.ic_baseline_play_arrow_24)
+            }
         }
-
         stop_btn.setOnClickListener {
-            intent = Intent(this,MyService::class.java)
-            intent.putExtra("Service1","stop")
-            play_btn.setImageResource(R.drawable.ic_baseline_play_arrow_24)
-            startService(intent)
+            Intent(applicationContext, MyService::class.java).apply {
+                stopService(this)
+            }
         }
 
+        forwardbutton.setOnClickListener {
+            Intent(applicationContext, MyService::class.java).putExtra(
+                MyService.Service1,
+                MyService.ForWard
+            ).apply {
+                startService(this)
+            }
+        }
+        backwardbutton.setOnClickListener {
+            Intent(applicationContext, MyService::class.java).putExtra(
+                MyService.Service1,
+                MyService.BackWard
+            ).apply {
+                startService(this)
+            }
+        }
     }
 }
--- app/src/main/java/com/example/practical5/MyService.kt
+++ app/src/main/java/com/example/practical5/MyService.kt
@@ -3,46 +3,94 @@ package com.example.practical5
 import android.app.Service
 import android.content.Intent
 import android.media.MediaPlayer
+import android.os.Handler
 import android.os.IBinder
-import android.view.LayoutInflater
-import android.widget.ImageButton
+import android.util.Log
+import android.widget.Toast
 
-
-class MyService : Service() {
-
-    lateinit var mediaPlayer: MediaPlayer
-    lateinit var play_btn : ImageButton
-    val Tag = "MyService"
-
-    override fun onBind(p0: Intent?): IBinder? {
-        TODO("Not yet implemented")
+class MyService: Service() {
+    companion object{
+        val PlayPause="play/pause"
+        val ForWard="forward"
+        val BackWard="backward"
+        val Service1="Service"
+        var startTimeS=0;
+        var finalTimeS=0;
+        var currentTimeS=0;
+    }
+    private var startTime = 0
+    private var finalTime = 0
+    private val forwardTime = 5000
+    private val backwardTime = 5000
+    private lateinit var mediaPlayer: MediaPlayer
+    private var handler = Handler()
+    private var runnable= Runnable(){
+        if (!this::mediaPlayer.isInitialized) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.song)
+            currentTimeS = mediaPlayer.getCurrentPosition()
+            Log.e("Current Time",""+ currentTimeS)
+            playHandler()
+        }
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-
-
-        if (!this::mediaPlayer.isInitialized) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.song)
+        if (!this::mediaPlayer.isInitialized){
+            mediaPlayer=MediaPlayer.create(this,R.raw.song)
+            finalTime = mediaPlayer.getDuration()
+            startTime = mediaPlayer.getCurrentPosition()
+            startTimeS=startTime;
+            finalTimeS= finalTime;
         }
-        if (intent!=null) {
-            val str = intent.getStringExtra("Service1")
-            if (str == "play"){
-                if (!mediaPlayer.isPlaying){
-                    mediaPlayer.start()
-                } else {
+        if (intent!=null){
+            val str1=intent.getStringExtra(Service1)
+            if (str1==PlayPause){
+                if (mediaPlayer.isPlaying){
                     mediaPlayer.pause()
+                    pauseHandler()
                 }
-            } else {
-                mediaPlayer.stop()
-                mediaPlayer = MediaPlayer.create(this, R.raw.song)
+                else{
+                    mediaPlayer.start()
+                    playHandler()
+                }
+            }else if(str1== ForWard){
+                val temp = startTime
+                if((temp+forwardTime)<=finalTime){
+                    startTime = startTime + forwardTime
+                    mediaPlayer.seekTo( startTime)
+                    Toast.makeText(applicationContext,"You have Jumped forward 5 seconds",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(applicationContext,"Cannot jump forward 5 seconds",Toast.LENGTH_SHORT).show()
+                }
+            }else if(str1== BackWard){
+                val temp = startTime
+                if((temp-backwardTime)>0){
+                    startTime = startTime - backwardTime;
+                    mediaPlayer.seekTo( startTime)
+                    Toast.makeText(getApplicationContext(),"You have Jumped backward 5 seconds",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(getApplicationContext(),"Cannot jump backward 5 seconds",Toast.LENGTH_SHORT).show()
+                }
             }
         }
-        return super.onStartCommand(intent, flags, startId)
+        else{
+            mediaPlayer.start()
+            playHandler()
+        }
+        return START_STICKY
     }
-
     override fun onDestroy() {
         mediaPlayer.stop()
         super.onDestroy()
     }
-}
 
+    fun playHandler(){
+        handler.postDelayed(runnable,1000)
+    }
+    fun pauseHandler(){
+        handler.removeCallbacks(runnable)
+    }
+
+    override fun onBind(p0: Intent?): IBinder? {
+        return null
+    }
+}
